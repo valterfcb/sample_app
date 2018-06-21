9179da1c7e11710796a748aee786771a3edce75c
Válter Bento <valter.fcb.wdd@gmail.com>
2018-06-21 12:54:45 +0100

    Update sample with 4 views and the correspondent test and dry code

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
   get 'foo/bar'
   get 'foo/baz'
 
@@ -8,5 +10,4 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
 end
