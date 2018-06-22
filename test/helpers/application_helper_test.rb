f12f8f1510bdab19fb7617a6e8cd79d230747d49
Válter Bento <valter.fcb.wdd@gmail.com>
2018-06-22 12:03:28 +0100

    Add layout link test

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+  test "full title helper" do
+    assert_equal full_title,        "Ruby on Rails" 
+    assert_equal full_title("Home"), "Home | Ruby on Rails"
+    assert_equal full_title("Help"), "Help | Ruby on Rails"
+    assert_equal full_title("About"), "About | Ruby on Rails"
+    assert_equal full_title("Contact"), "Contact | Ruby on Rails"
+  end
+end
---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+	# test "the truth" do
+	#   assert true
+	# end
+
+
+	test "layout links" do
+		get root_path
+		assert_template 'static_pages/home'
+		assert_select "a[href=?]", root_path, count: 2
+		assert_select "a[href=?]", help_path
+		assert_select "a[href=?]", about_path
+		assert_select "a[href=?]", contact_path
+    #Nao estou a perceber estas duas linhas de codigo abaixo do comentario?????, sao
+    #correspondentes a Lisnting 5.36 do rails tutorial
+    get contact_path
+    assert_select "title", full_title("Contact")
+	end
+
+end
