3bb45b56ec00ebd90b49295dd2e3ec1fc4ad70ed
Válter Bento <valter.fcb.wdd@gmail.com>
2018-06-22 12:19:16 +0100

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -31,4 +31,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
    assert_select "title", "Contact | #{@base_title}"
   end
 
+test "should get sign up" do
+    get signup_path
+   assert_response :success
+   assert_select "title", "Sign up | #{@base_title}"
+end
+
+
 end
